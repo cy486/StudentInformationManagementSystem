9a0f005e22385911fb9b1f3193fb7e695baa7e3a
Necydcy <necydcy@gamil.com>
2019-06-01 01:02:02 +0800

    1
    first

---
+++ src/com/score/dao/impl/SystemDaoImpl.java
@@ -0,0 +1,29 @@
+package com.score.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import com.score.bean.Clazz;
+import com.score.bean.Grade;
+import com.score.bean.Student;
+import com.score.dao.inter.SystemDaoInter;
+import com.score.tools.MysqlTool;
+
+/**
+ * 系统数据层
+ * @author bojiangzhou
+ *
+ */
+public class SystemDaoImpl extends BaseDaoImpl implements SystemDaoInter {
+
+	
+	
+	
+	
+}
---
+++ src/com/score/dao/inter/SystemDaoInter.java
@@ -0,0 +1,8 @@
+package com.score.dao.inter;
+
+import java.util.List;
+
+public interface SystemDaoInter extends BaseDaoInter {
+	
+	
+}
